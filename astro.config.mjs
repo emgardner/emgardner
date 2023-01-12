a3563b349c1bfcdb680b80a323426f29f90fc9e0
ETHAN GARDNER <gardner.ethan10@gmail.com>
2023-01-12 07:29:46 -0500

    Commiting Tailwind Features

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,13 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(), 
+    tailwind({})
+  ],
   site: 'https://emgardner.github.io'
 });
